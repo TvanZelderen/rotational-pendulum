1c71ae52b9b5fc97566370208af27ea8f7d12ca0
Thomas_vz <103991671+TvanZelderen@users.noreply.github.com>
2026-05-01 11:31:54 +0200

    First real commit, sample from homepage

---
+++ first-script.m
@@ -0,0 +1,43 @@
+%% Sample code to run experiment from script,
+% specifying a fixed input signal and recording the measured
+% outputs to a variable in the Workspace for further processing.
+
+clear;clc;
+
+% Sample rate in sec.
+h = 0.01;
+
+% Experiment duration in sec.
+% (don't forget to change this in your diagram, see video)
+Tsim = 15;
+
+% Time vector (don't forget to transpose with ')
+t = [0:h:Tsim]';
+
+% Input vector
+amplitude = 0.3;
+omega = 1;
+u = amplitude * sin(omega * t);
+
+% Variable that goes to Simulink
+% (First column: time, Second column: input values)
+simin = [t, u]
+
+%% Start experiment
+sim rotpentemplate
+
+%% Collect output data
+% (make sure that samples are taken every 'h' seconds! in 'To Workspace' block)
+
+% If output type 'Timeseries'
+y = simout.Data;
+
+% If output type 'Array'
+% y = simout;
+
+%% Plot data
+th1 = y(:, 1);
+th2 = y(:, 2);
+
+plot(t, th1, t, th2)
+legend('theta_1', 'theta_2')
