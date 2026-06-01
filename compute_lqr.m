24890df0a446f0884f88e158eda29f73d736f756
Vincentmaan <150692915+Vincentmaan@users.noreply.github.com>
2026-06-01 13:33:44 +0200

    L term added

--- compute_lqr.m
+++ compute_lqr.m
@@ -1,4 +1,4 @@
-function K = compute_lqr(x0, R, p)
+function [K,L] = compute_lqr(x0, R, p)
 % Returns LQR gain for operating point x0.
 % Bryson Q: x_max = [pi, 8, pi, 26.18] rad (or rad/s)
 
@@ -25,3 +25,5 @@ R_obs = diag([1, 1]);
 L = lqr(A', C', Q_obs, R_obs);
 
 end
+
+
