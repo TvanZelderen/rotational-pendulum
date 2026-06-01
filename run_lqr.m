76dcd5fc88f2d4152c02a477635b38e2086247c6
Thomas <103991671+TvanZelderen@users.noreply.github.com>
2026-06-01 17:20:16 +0200

    Extra diagnosis

--- run_lqr.m
+++ run_lqr.m
@@ -32,18 +32,25 @@ odth2 = y(:,9);
 input  = y(:,10);
 e_vec = y(:,11:14);   % controller error ref - x, all 4 states (rad)
 
-  figure(4); clf;
-  subplot(3,2,1); plot(t_out, th1, t_out, oth1); yline(180,'k--');
-      legend('\theta_1','\theta_1 obs'); ylabel('\theta_1 [deg]'); grid on;
-  subplot(3,2,2); plot(t_out, th2, t_out, oth2);
-      legend('\theta_2','\theta_2 obs'); ylabel('\theta_2 [deg]'); grid on;
-  subplot(3,2,3); plot(t_out, dth1, t_out, odth1);
-      legend('d\theta_1','d\theta_1 obs'); ylabel('d\theta_1'); grid on;
-  subplot(3,2,4); plot(t_out, dth2, t_out, odth2);
-      legend('d\theta_2','d\theta_2 obs'); ylabel('d\theta_2'); grid on;
-  subplot(3,2,5); plot(t_out, input); yline(0.5,'r--'); yline(-0.5,'r--');
-      ylabel('u'); xlabel('Time [s]'); grid on;
-  subplot(3,2,6); plot(t_out, e_vec);
-      legend('e_{\theta1}','e_{d\theta1}','e_{\theta2}','e_{d\theta2}');
-      ylabel('error [rad]'); xlabel('Time [s]'); grid on;
-  sgtitle('LQR');
+figure(1); clf;
+subplot(3,2,1); plot(t_out, th1, t_out, oth1); yline(180,'k--');
+  legend('\theta_1','\theta_1 obs'); ylabel('\theta_1 [deg]'); grid on;
+subplot(3,2,2); plot(t_out, th2, t_out, oth2);
+  legend('\theta_2','\theta_2 obs'); ylabel('\theta_2 [deg]'); grid on;
+subplot(3,2,3); plot(t_out, dth1, t_out, odth1);
+  legend('d\theta_1','d\theta_1 obs'); ylabel('d\theta_1'); grid on;
+subplot(3,2,4); plot(t_out, dth2, t_out, odth2);
+  legend('d\theta_2','d\theta_2 obs'); ylabel('d\theta_2'); grid on;
+subplot(3,2,5); plot(t_out, input); yline(0.5,'r--'); yline(-0.5,'r--');
+  ylabel('u'); xlabel('Time [s]'); grid on;
+subplot(3,2,6); plot(t_out, e_vec);
+  legend('e_{\theta1}','e_{d\theta1}','e_{\theta2}','e_{d\theta2}');
+  ylabel('error [rad]'); xlabel('Time [s]'); grid on;
+sgtitle('LQR');
+
+e_th2 = y(:,13);
+figure(2); clf;
+yyaxis left; plot(t_out, input); label('u [-]');
+yyaxis right; plot(t_out, e_th2); label('e_{\theta2} [deg]');
+xlabel('Time [s'); grid on; legend('u', 'e_{\theta2}');
+
