393a93abe6fe7511bf18f04adb2c7c47d8da185e
Thomas <103991671+TvanZelderen@users.noreply.github.com>
2026-06-01 16:21:41 +0200

    lqr data, gains and offsets corrected

--- run_lqr.m
+++ run_lqr.m
@@ -6,7 +6,7 @@ hwinit;
 pendulum_params;
 
 h = 0.001;
-R = 60;
+R = 500;
 ref = [pi; 0; 0; 0];
 [K, L, A, B, C] = compute_lqr(ref, R, p);
 
@@ -31,9 +31,10 @@ odth2 = y(:,9);
 input  = y(:,10);
 error = y(:,11);
 
-figure(3); clf;
-    subplot(3,1,1); plot(t_out, th1, t_out, oth1); legend('\theta_1', '\theta_1_ob'); ylabel('\theta_1 [deg]'); grid on;
-    subplot(3,1,2); plot(t_out, dth1, t_out, odth1); legend('\theta_1_d', '\theta_1_dob'); ylabel('d\theta_1'); grid on;
-    subplot(3,1,3); plot(t_out, input, t_out, error); legend('input', 'error'); ylabel('Controller'); grid on;
+figure(4); clf;
+    subplot(4,1,1); plot(t_out, th1, t_out, oth1); legend('\theta_1', '\theta_1_ob'); ylabel('\theta_1 [deg]'); grid on;
+    subplot(4,1,2); plot(t_out, dth1, t_out, odth1); legend('\theta_1_d', '\theta_1_dob'); ylabel('d\theta_1'); grid on;
+    subplot(4,1,3); plot(t_out, th2, t_out, oth2); legend('\theta_2', '\theta_2_ob'); ylabel('\theta_1 [deg]'); grid on;
+    subplot(4,1,4); plot(t_out, input, t_out, error); legend('input', 'error'); ylabel('Controller'); grid on;
     xlabel('Time [s]');
     sgtitle(sprintf('LQR'));
