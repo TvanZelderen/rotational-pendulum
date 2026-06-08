1e1e9b596c2c5e87e6baad3cdfb7dbd3a7b45b57
Vincentmaan <150692915+Vincentmaan@users.noreply.github.com>
2026-06-08 14:55:36 +0200

    starting points for MPC

--- compute_mpc.m
+++ compute_mpc.m
@@ -31,3 +31,7 @@ mpc_obj.MV.Max = 1;  % Max
 mpc_obj.Weights.ManipulatedVariables = R;         % 'R' penalty (motor effort)
 mpc_obj.Weights.ManipulatedVariablesRate = 0.1;   % Penalty on violently changing voltage
 mpc_obj.Weights.OutputVariables = [10, 0, 10, 0]; % 'Q' penalty (positions)
+
+mpc_obj.Model.Nominal.X = [0; 0; pi; 0]; % The 4 internal states
+mpc_obj.Model.Nominal.Y = [0; pi];       % The 2 physical outputs (sensors)
+mpc_obj.Model.Nominal.U = 0;
