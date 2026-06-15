8100bef359900b9212fd8465c08017e06d767753
Thomas <103991671+TvanZelderen@users.noreply.github.com>
2026-06-15 09:17:16 +0200

    Tweaking continues, still offset from the top.

--- run_lqr.m
+++ run_lqr.m
@@ -37,12 +37,12 @@ if reference_indicator == 1
     offset = [0; 0];
     R = 50;
 elseif reference_indicator == 2
-    ref = [0; 0; pi; 0];
-    offset = [0; -pi];
+    ref = [0; 0; psi; 0];
+    offset = [0; -psi];
     R = 25;
 elseif reference_indicator == 3
-    ref = [-pi; 0; 0; 0];
-    offset = [pi; 0];
+    ref = [-psi; 0; 0; 0];
+    offset = [psi; 0];
     R = 100;
 end
 
@@ -84,30 +84,24 @@ th1 = y(:,1);
 dth1 = y(:,2);
 th2 = y(:,3);
 dth2 = y(:,4);
-phi = y(:,1) + y(:,3);
+psi = y(:,5);
 input  = y(:,6);
 
 figure(1); clf;
-subplot(3,2,1); plot(t_out, th1, t_out, oth1); yline(180,'k--');
-  legend('\theta_1','\theta_1 obs'); ylabel('\theta_1 [deg]'); grid on;
-subplot(3,2,2); plot(t_out, th2, t_out, oth2);
-  legend('\theta_2','\theta_2 obs'); ylabel('\theta_2 [deg]'); grid on;
-subplot(3,2,3); plot(t_out, dth1, t_out, odth1);
-  legend('d\theta_1','d\theta_1 obs'); ylabel('d\theta_1'); grid on;
-subplot(3,2,4); plot(t_out, dth2, t_out, odth2);
-  legend('d\theta_2','d\theta_2 obs'); ylabel('d\theta_2'); grid on;
-subplot(3,2,5); plot(t_out, input); yline(0.5,'r--'); yline(-0.5,'r--');
+subplot(3,2,1); plot(t_out, th1);
+  legend('\theta_1'); ylabel('\theta_1 [deg]'); grid on;
+subplot(3,2,2); plot(t_out, th2);
+  legend('\theta_2'); ylabel('\theta_2 [deg]'); grid on;
+subplot(3,2,3); plot(t_out, dth1);
+  legend('d\theta_1'); ylabel('d\theta_1'); grid on;
+subplot(3,2,4); plot(t_out, dth2);
+  legend('d\theta_2'); ylabel('d\theta_2'); grid on;
+subplot(3,2,5); plot(t_out, psi); yline(0.5,'r--'); yline(-0.5,'r--');
   ylabel('u'); xlabel('Time [s]'); grid on;
-subplot(3,2,6); plot(t_out, e_vec);
-  legend('e_{\theta1}','e_{d\theta1}','e_{\theta2}','e_{d\theta2}');
+subplot(3,2,6); plot(t_out, input);
+  legend('e_{input}');
   ylabel('error [rad]'); xlabel('Time [s]'); grid on;
 sgtitle('LQR');
-
-e_th2 = y(:,13);
-figure(2); clf;
-yyaxis left;  plot(t_out, input);  ylabel('u [-]');
-yyaxis right; plot(t_out, e_th2);  ylabel('e_{\theta2} [rad]');
-xlabel('Time [s]'); grid on; legend('u','e_{\theta2}');
 
 %% -- Jiggle diagnostics (cols 15-16; present only after Simulink wiring) --
 % simout col 15 = jiggle [-]   col 16 = stuck [bool]
